9bc371447f6c9a1c34bdc477334818bcf43b1de5
Felipe <felipematheus17@hotmail.com>
2026-01-02 14:34:29 -0300

    :shirt: feat: rebuild login form and increnment new layout this login page

--- src/app/login/_components/login-form.tsx
+++ src/app/login/_components/login-form.tsx
@@ -9,15 +9,25 @@ import * as z from "zod";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useAuth } from "@/providers/auth-provider";
+import { Loader } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Card, CardContent } from "@/components/ui/card";
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
+import Image from "next/image";
+import loginImage from "@/assets/fundo-login.png";
+import { toast } from "sonner";
 
 const loginFormSchema = z.object({
-  email: z.string().email("E-mail inválido.").min(1, "E-mail é obrigatório."),
+  email: z.email("E-mail inválido.").min(1, "E-mail é obrigatório."),
   password: z.string().min(1, "Senha é obrigatória."),
 });
 
 type LoginFormInputs = z.infer<typeof loginFormSchema>;
 
-export function LoginForm() {
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
   const { signin, isLogged, loading } = useAuth();
   const router = useRouter();
 
@@ -38,7 +48,11 @@ export function LoginForm() {
   }, [isLogged, router]);
 
   if (loading || isLogged) {
-    return <div>Carregando...</div>;
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="animate-spin w-12 h-12" />
+      </div>
+    );
   }
 
   async function onSubmit(data: LoginFormInputs) {
@@ -46,10 +60,10 @@ export function LoginForm() {
     try {
       await signin(data.email, data.password);
       router.push("/dashboard");
-    } catch (error: any) {
-      // Handle backend errors
-      if (error.message) {
+    } catch (error: unknown) {
+      if (error instanceof Error) {
         setBackendError(error.message);
+        toast.error(error.message);
       } else {
         setBackendError("Ocorreu um erro inesperado. Tente novamente.");
       }
@@ -57,27 +71,54 @@ export function LoginForm() {
   }
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="w-96 space-y-4 border p-6 rounded-lg"
-    >
-      <h1 className="text-xl font-semibold">Login</h1>
-
-      <Input placeholder="Email" {...register("email")} />
-      {errors.email && (
-        <p className="text-red-500 text-sm">{errors.email.message}</p>
-      )}
-
-      <Input type="password" placeholder="Senha" {...register("password")} />
-      {errors.password && (
-        <p className="text-red-500 text-sm">{errors.password.message}</p>
-      )}
-
-      {backendError && <p className="text-red-500 text-sm">{backendError}</p>}
-
-      <Button className="w-full" type="submit" disabled={isSubmitting}>
-        Entrar
-      </Button>
-    </form>
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form className="p-6 md:p-8" onSubmit={handleSubmit(onSubmit)}>
+            <FieldGroup>
+              <div className="flex flex-col items-center gap-2 text-center">
+                <h1 className="text-2xl font-bold">Bem-vindo(a) de volta</h1>
+                <p className="text-muted-foreground text-balance">
+                  Digite seu e-mail e senha para acessar sua conta.
+                </p>
+              </div>
+              <Field>
+                <FieldLabel htmlFor="email">Email:</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                  {...register("email")}
+                />
+                {errors.email && toast.error(errors.email.message)}
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="password">Senha:</FieldLabel>
+                <Input
+                  id="password"
+                  type="password"
+                  required
+                  {...register("password")}
+                />
+                {errors.password && toast.error(errors.password.message)}
+              </Field>
+              <Field>
+                <Button type="submit" disabled={isSubmitting}>
+                  {isSubmitting ? "Entrando..." : "Entrar"}
+                </Button>
+              </Field>
+            </FieldGroup>
+          </form>
+          <div className="bg-muted relative h-64 md:h-auto">
+            <Image
+              src={loginImage}
+              alt="Fundo Login"
+              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+            />
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,9 @@
 import { LoginForm } from "./_components/login-form";
 
 export default function LoginPage() {
-  
   return (
-    <div className="h-screen flex items-center justify-center">
-      <LoginForm />
+    <div className="h-screen flex items-center justify-center p-6 md:p-10">
+      <LoginForm className="w-full max-w-sm md:max-w-3xl" />
     </div>
   );
 }
